0bdd0ad08e928c61b338407e5bc9dae552f0b6b5
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-08-26 10:04:16 -0300

    start JavaScript studies

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+const firstName = 'Sérgio'
+const lastName = 'Sanchez'
+
+const name = firstName + ' ' + lastName
+const age = 32
+
+console.log(name)
+console.log(age)
+
+// Concatenação de string e number
+console.log(name + age)
+
+// Concatenação
+console.log('Eu, ' + name + ' tenho ' + age + ' anos')
+
+// Template string
+console.log(`Eu, ${name} tenho ${age} anos e ${1 + 3} meses`)
