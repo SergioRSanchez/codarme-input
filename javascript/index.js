681a3ecb39c2f506b610b3102b23f3235db45134
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-08-26 11:36:47 -0300

    var e list

--- javascript/index.js
+++ javascript/index.js
@@ -20,3 +20,24 @@ console.log(`Eu, ${name} tenho ${age} anos e ${1 + 3} meses`)
 const guests = (['João', 'Maria', 'Pedro', [name, age]])
 console.log(guests)
 console.log(guests[1])
+
+// Definição / Declaração de variável
+let x
+// Atribuição de valor
+x = 10
+console.log(x)
+x = 20
+console.log(x)
+
+// Acessando dados dentro de listas multidimensionais
+const guests2 = [
+  ['João', true],
+  ['Maria', false]
+]
+
+console.log(guests2)
+console.log(guests2[0][0])
+
+// Atribuindo um valor de uma lista a uma nova variável
+const guestJoao = guests2[0]
+console.log(guestJoao)
