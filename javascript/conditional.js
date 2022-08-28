793dbbc21a423cfc7781c45b0410d53bf39cd940
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-08-28 15:24:17 -0300

    conditions JS

---
+++ javascript/conditional.js
@@ -0,0 +1,14 @@
+console.log('Iniciando')
+
+const lightIsGreen = false
+const lightIsYellow = true
+
+if (lightIsGreen) {
+  console.log('O semáforo está VERDE, passe')
+} else if (lightIsYellow) {
+  console.log('O semáforo está AMARELO, atenção')
+} else {
+  console.log('O semáforo está VERMELHO, pare')
+}
+
+console.log('Finalizando')
