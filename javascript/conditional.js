98e43035587a7d9dc28bbf389c1ace5b906ba53f
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-08-29 12:48:28 -0300

    update JS studies

--- javascript/conditional.js
+++ javascript/conditional.js
@@ -37,3 +37,52 @@ switch (semaforo) {
     break
   default: console.log('Semáforo DESLIGADO')
 }
+
+
+console.log('-----Operadores de Comparação-----')
+console.log(33 == '33')  //compara os valores
+console.log(33 === '33')  //compara os tipos
+console.log(33 > 32)  //maior que
+console.log(33 >= 33)  //maior ou igual
+console.log(33 < 32)  //menor que
+console.log(33 <= 33)  //menor ou igual
+console.log(33 != '33')  //diferente sem comparar o tipo
+console.log(33 !== 33)  //diferente comparando o tipo também
+
+
+console.log('-----Operadores Lógicos-----')
+//  AND  &&
+const person = {
+  name: 'Maria',
+  age: 17,
+  guest: true
+}
+const mariaPodeEntrar = person.guest === true && person.age >= 18
+if (mariaPodeEntrar) {
+  console.log('Bem vindo')
+} else {
+  console.log('Entrada Proibida')
+}
+
+// OR  ||
+const person2 = {
+  name: 'Jão',
+  child: true,
+  guest: false
+}
+if (person2.guest === true || person2.child === true) {
+  console.log('Bem vindo')
+} else {
+  console.log('Entrada Proibida')
+}
+
+// NOT  !
+const negativo = !true
+console.log(negativo)
+
+
+const y = 'Sergio' && 0 && 'Sanchez'  //  Retorna o Primeiro Falsy ou último Truthy
+console.log(y)
+
+const z = 0 || 'Sergio' || 'Sanchez'  //  Retorna o Primeiro Truthy
+console.log(z)
