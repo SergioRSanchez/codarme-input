10dba23cf3f4d90ed400901a19de2959e744fb54
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-09-08 14:56:27 -0300

    JS modulo 17 exercises

--- javascript/modulo17/exercicio.js
+++ javascript/modulo17/exercicio.js
@@ -5,6 +5,18 @@
 - Utilizando a função "forEach" nativa, percorra a lista e mostre no console se o número é par ou ímpar. Exemplo: "O número 4 é par" */
 console.log('-----Exercício 1-----')
 
+const exercicio1 = [1, 2, 3, 4, 5, 6, 7]
+
+function parOuImpar(numero) {
+  if (numero % 2 == 0) {
+    console.log(`O número ${numero} é par`)
+  } else {
+    console.log(`O número ${numero} é ímpar`)
+  }
+}
+
+exercicio1.forEach(parOuImpar)
+
 
 /* 2. Listas derivadas
 
@@ -12,6 +24,25 @@ console.log('-----Exercício 1-----')
 
 - Mostre no console a quantidade de números pares e a quantidade de ímpares */
 console.log('-----Exercício 2-----')
+
+const listaPar = exercicio1.filter((numero) => {
+  if (numero % 2 == 0) {
+    return true
+  } else {
+    return false
+  }
+})
+const listaImpar = exercicio1.filter((numero) => {
+  if (numero % 2 != 0) {
+    return true
+  } else {
+    return false
+  }
+})
+console.log(listaPar.length)
+console.log(listaPar)
+console.log(listaImpar.length)
+console.log(listaImpar)
 
 
 /* 3. Alterando valores
@@ -22,6 +53,15 @@ console.log('-----Exercício 2-----')
 
 - Os valores ímpares devem ser multiplicados por 3 */
 console.log('-----Exercício 3-----')
+
+const multiplicaPar = listaPar.map(item => {
+  return item * 2
+})
+const multiplicaImpar = listaImpar.map(item => {
+  return item * 3
+})
+console.log(multiplicaPar)
+console.log(multiplicaImpar)
 
 
 /* 4. Simulando forEach
