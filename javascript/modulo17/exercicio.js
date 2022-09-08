6dd0487903dfb1c0a3c4c3c26499e029df4fcec6
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-09-08 11:58:46 -0300

    JS modulo 17 exercises

---
+++ javascript/modulo17/exercicio.js
@@ -0,0 +1,54 @@
+/* ​1. Par ou ímpar?
+
+- Declare uma variável que receba um array com 7 números
+
+- Utilizando a função "forEach" nativa, percorra a lista e mostre no console se o número é par ou ímpar. Exemplo: "O número 4 é par" */
+console.log('-----Exercício 1-----')
+
+
+/* 2. Listas derivadas
+
+- Baseado no código do exercício anterior, crie duas outras listas, uma com números pares, outra com números ímpares, utilizando "filter"
+
+- Mostre no console a quantidade de números pares e a quantidade de ímpares */
+console.log('-----Exercício 2-----')
+
+
+/* 3. Alterando valores
+
+- Utilize o método map para criar uma nova lista
+
+- Os valores pares devem ser multiplicados por 2
+
+- Os valores ímpares devem ser multiplicados por 3 */
+console.log('-----Exercício 3-----')
+
+
+/* 4. Simulando forEach
+
+- Declare uma função chamada forEach que recebe dois parâmetros: um array e um callback
+
+- Use a estrutura "for" para percorrer o array e executar o callback para cada um dos itens da lista
+
+- Execute forEach passando uma lista de 7 números, e um callback que mostre a mensagem "O número NUMERO é par/ímpar". */
+console.log('-----Exercício 4-----')
+
+
+/* 5. Somando e reduzindo
+
+- Utilize reduce reduzir a lista do exercício 1
+
+- Você deve usar soma quando o valor for par, e multiplicação quando o valor for ímpar
+
+- Mostre no console o valor final
+
+- Verifique, manualmente (no papel ou na calculadora) se o valor final está correto. */
+console.log('-----Exercício 5-----')
+
+
+/* 6. Cifra de Cezar
+
+- Refaça o exercício da aula 8 do módulo 1, dessa vez usando JS
+
+- Compartilhe no canal de exercícios no discord, usando a hashtag #exercicioMapaConversãoJS */
+console.log('-----Exercício 6-----')
