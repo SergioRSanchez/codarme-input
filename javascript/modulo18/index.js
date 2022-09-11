ee85669defdf8f36d6df5e752d00726828144b6d
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-09-11 16:17:47 -0300

    JS studies modulo 18 - DOM

--- javascript/modulo18/index.js
+++ javascript/modulo18/index.js
@@ -1,1 +1,28 @@
-console.log(window.document.getElementById('app'))
+const containerRoot = window.document.getElementById('app')
+console.log(containerRoot)
+console.log(containerRoot.attributes)
+console.log(containerRoot.attributes.x)
+console.log(containerRoot.hasAttribute('class'))
+console.log(containerRoot.hasAttribute('z'))
+console.log(window.document.getElementsByTagName('div'))
+
+
+console.log('Manipulando o elemento - mudando o conteúdo')
+containerRoot.innerHTML = 'Olá HTML'
+
+
+console.log('Criando e Adicionando Elemento')
+const title = window.document.createElement('h1')  // Criando um elemento com tag h1
+title.setAttribute('id', 'title')  // Adicionando um atributo id = 'title'
+title.innerText = 'Olá DOM'  // Adicionando um texto
+containerRoot.appendChild(title)  // Inserindo o elemento
+
+
+console.log('Trabalhando com eventos')
+const inputN1 = document.getElementById('n1')  // podemos suprimir o uso do window, que irá funcionar tbm
+const inputN2 = document.getElementById('n2')
+
+inputN1.onblur = function () {
+  console.log('INPUT', inputN1.value)
+}
+
