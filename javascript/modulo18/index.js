08d3270ec217bc18fd64cc38808db77bc22ecfe7
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-09-12 22:00:40 -0300

    JS studies modulo 18 DOM

--- javascript/modulo18/index.js
+++ javascript/modulo18/index.js
@@ -7,18 +7,18 @@ console.log(containerRoot.hasAttribute('z'))
 console.log(window.document.getElementsByTagName('div'))
 
 
-console.log('Manipulando o elemento - mudando o conteúdo')
+//  Manipulando o elemento - mudando o conteúdo
 containerRoot.innerHTML = 'Olá HTML'
 
 
-console.log('Criando e Adicionando Elemento')
+//  Criando e Adicionando Elemento
 const title = window.document.createElement('h1')  // Criando um elemento com tag h1
 title.setAttribute('id', 'title')  // Adicionando um atributo id = 'title'
 title.innerText = 'Olá DOM'  // Adicionando um texto
 containerRoot.appendChild(title)  // Inserindo o elemento
 
 
-console.log('Trabalhando com eventos, criando uma calculadora de soma')
+//  Trabalhando com eventos, criando uma calculadora de soma
 const inputN1 = document.getElementById('n1')  // podemos suprimir o uso do window, que irá funcionar tbm
 const inputN2 = document.getElementById('n2')
 const button = document.getElementById('btn')
@@ -29,7 +29,8 @@ resultElement.innerText = 'Não há valor ainda'
 resultElement.style.backgroundColor = 'yellow'
 appElement.appendChild(resultElement)
 
-button.onclick = function () {
+button.onclick = function (event) {
+  console.log(event)
   const n1Value = Number(inputN1.value)
   const n2Value = Number(inputN2.value)
   const result = n1Value + n2Value
@@ -44,10 +45,16 @@ inputN2.onblur = function () {
 }
 
 
-console.log('Mais de uma função por evento')
-
+// Mais de uma função por evento
 const ev01 = () => console.log('1')
 const ev02 = () => console.log('2')
 button.addEventListener('click', ev01)
 button.addEventListener('click', ev02)
 button.removeEventListener('click', ev01)  // removemos o evento 1
+
+
+//  Prevenindo um evento padrão
+const formElement = document.getElementById('formulario')
+formElement.onsubmit = (event) => {
+  event.preventDefault()
+}
