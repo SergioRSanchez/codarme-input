03515f90ea02bd31201721fa4bed85beb4921b17
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-09-15 19:27:30 -0300

    JS studie modulo 19 Modern JS

--- javascript/modulo19/index.js
+++ javascript/modulo19/index.js
@@ -13,3 +13,30 @@ console.log(sum(listaSpread[0], listaSpread[1]))
 
 
 console.log('----- Destructuring Assignment -----')
+const listaDestructuring = [1, 2, 3, 4, 5]
+const item1 = listaDestructuring.shift()  //  Retiramos o primeiro item da lista e inserimos na variável
+console.log(listaDestructuring, item1)
+
+const [item, ...listaDestructuring2] = [1, 2, 3, 4, 5]  //  Utilizamos o Destructuring Assignment
+console.log(listaDestructuring2, item)
+
+const person = {
+  name: 'Sergio',
+  age: 32,
+  company: 'Sanchez'
+}
+// const name = person.name
+// const age = person.age
+// console.log(name, age)  //  Maneira que havíamos visto anteriormente
+
+const { name, age } = person  //  Utilizando Destructuring Assignment
+console.log(name, age)
+
+
+//  Ou fazemos direto no objeto
+const { age2, ...others } = {
+  name2: 'Sergio Roberto',
+  age2: 32,
+  company2: 'Sanchez Filho'
+}
+console.log(age2, others)
