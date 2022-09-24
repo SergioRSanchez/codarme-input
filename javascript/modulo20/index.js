043912e63cdb06543f04cf5c49ba8b6a57db2c0f
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-09-24 18:25:59 -0300

    JS study sync and async - try/catch

--- javascript/modulo20/index.js
+++ javascript/modulo20/index.js
@@ -29,16 +29,38 @@ console.log('finalizou aqui novamente')
 //  Para fazer uma promise, nós devemos passar duas funções como parâmetros, uma caso Sucesso e um caso Erro
 //  para chamar a função de sucesso(resolve), usamos o .then
 //  para chamar a função de erro(reject), usamos o .catch
-const promise = new Promise((resolve, reject) => {
-  const sucesso = false  //  se true irá executar resolve, se false irá executar reject
-  if (sucesso) {
-    resolve('Resolve')
-  } else {
-    reject('Reject')
+function minhaPromise() {
+  const promise = new Promise((resolve, reject) => {
+    const sucesso = false  //  se true irá executar resolve, se false irá executar reject
+    setTimeout(() => {
+      if (sucesso) {
+        resolve('Resolve')
+      } else {
+        reject('Reject')
+      }
+    }, 3000);
+  })
+  return promise
+}
+
+async function main() {
+  console.log('ANTES')  //  para mostrar que foi executado ANTES da nossa promise
+
+  try {
+    const result = await minhaPromise()  // transformou em um código blocking
+    console.log('SUCESSO: ', result)
+  } catch (result) {
+    console.log('ERRO: ', result)
   }
-})
-promise.then((result) => {
-  console.log('SUCESSO: ', result)
-}).catch((result) => {
-  console.log('ERRO: ', result)
-})
+
+  //  O código acima é blocking (sync), e o código abaixo é non-blocking (async) 
+
+  // minhaPromise().then((result) => {  // o retorno minhaPromise() é a própria promise, daí posso fazer o .then e .catch
+  //   console.log('SUCESSO: ', result)
+  // }).catch((result) => {
+  //   console.log('ERRO: ', result)
+  // })
+
+  console.log('DEPOIS')  //  para mostrar que foi executado DEPOIS da nossa promise
+}
+main()
