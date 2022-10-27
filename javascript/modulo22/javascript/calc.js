d80d597ec251e7ef5e5d62c8ac704b37ab163292
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-10-26 22:02:01 -0300

    ESM and commonJS

--- javascript/modulo22/javascript/calc.js
+++ javascript/modulo22/javascript/calc.js
@@ -7,6 +7,10 @@ export function divisao(n1, n2) {
   return n1 / n2
 }
 
+export const primeira = 'primeira variável'
+
+export const segunda = 'segunda variável'
+
 
 //  default export
 export default function multiplicacao(n3, n4) {
