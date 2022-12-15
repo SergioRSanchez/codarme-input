5285458426127c18349623ee2286a16dbb59421d
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-12-14 22:44:27 -0300

    node studie

--- javascript/modulo23/usandoReact/node/index.js
+++ javascript/modulo23/usandoReact/node/index.js
@@ -1,33 +1,45 @@
 //  Pacote do Node para permanecer escutando as chamadas HTTP
 const http = require('http')
 
+
+//  Definindo nossa lista de usuários
+const users = [
+  { id: 1, name: 'Sergio' },
+  { id: 2, name: 'Julia' },
+]
+
+
 //  Criando o servidor
 const server = http.createServer((request, response) => {
-  //  Resposta do servidor para o navegador, devolvendo o código de status e os cabeçalhos
-  response.writeHead(200, {
-    'Content-type': 'text/html'
-  })
 
-  response.write(`
-    <!DOCTYPE html>
-    <html lang="pt-BR">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Hello React</title>
+  //  Definindo o Header
+  response.setHeader('Content-type', 'application/json')
 
-      <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js"></script>
-      <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+  //  Método POST e GET
+  if (request.method === 'POST') {
 
-    </head>
-    <body>
-      <div>Hello Http</div>
-    </body>
-    </html>
-  `)
+    //  Adiciona um item na lista
+    const user = { id: 3, name: "Fulano" }
+    users.push(user)
+
+    //  Devolvendo o código de status 201 (Created)
+    response.statusCode = 201
+
+    response.write(JSON.stringify(user))
+
+  } else {
+
+    //  Devolvendo o código de status 200 (OK)
+    response.statusCode = 200
+
+    //  Retorna a lista de usuários
+    response.write(JSON.stringify(users))
+
+  }
+
   response.end()
 })
+
 
 //  Faz o programa ficar o tempo todo escutando, e passamos a porta que queremos que escute
 //  Porta 80 é a padrão, e as vezes usamos a 3000 em Desenvolvimento
